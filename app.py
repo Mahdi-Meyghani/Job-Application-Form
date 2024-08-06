e229fd49cd4a69bf4f9cb0c2cb1bf8bbbccf299d
Mahdi <mahdimeyghani02@gmail.com>
2024-08-06 19:34:47 +0330

    Get the data by a POST REQUEST in app.py from the page

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
 @app.route("/", methods=["GET", "POST"])
 def index():
+    if request.method == "POST":
+        first_name = request.form["fname"]
+        last_name = request.form["lname"]
+        email = request.form["email"]
+        date = request.form["date"]
+        occupation = request.form["occupation"]
+
     return render_template("index.html")
 
 
